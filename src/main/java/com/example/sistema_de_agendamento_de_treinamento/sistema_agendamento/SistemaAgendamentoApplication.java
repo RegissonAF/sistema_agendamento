6b07d2a43b46c9718d357e485a1336fc774aea3d
Regisson Andre de Lara Filho <99519216+RegissonAF@users.noreply.github.com>
2024-11-12 09:14:00 -0300

    Criando Projeto

---
+++ src/main/java/com/example/sistema_de_agendamento_de_treinamento/sistema_agendamento/SistemaAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_de_agendamento_de_treinamento.sistema_agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAgendamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistema_de_agendamento_de_treinamento/sistema_agendamento/SistemaAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_de_agendamento_de_treinamento.sistema_agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
